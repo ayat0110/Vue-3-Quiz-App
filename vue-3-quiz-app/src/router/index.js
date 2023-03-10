59c503054ec4aac2457a64091885126494d153bf
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2023-03-10 23:40:47 +0300

    questions feature added

--- vue-3-quiz-app/src/router/index.js
+++ vue-3-quiz-app/src/router/index.js
@@ -1,5 +1,6 @@
 import {createRouter, createWebHistory} from "vue-router"
 import QuizesView from "../views/QuizesView.vue"
+import Quiz from "../views/QuizView.vue"
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
@@ -7,6 +8,12 @@ const router = createRouter({
             path:"/",
             name:'quizes',
             component: QuizesView
+
+        },
+        {
+            path: "/quiz/:id",
+            name: 'quiz',
+            component: Quiz
 
         }
     ]
---
+++ vue-3-quiz-app/src/views/QuizView.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <header>
+            <h4>Question 1/3</h4>
+            <div class="bar">
+                <div class="completion"></div>
+            </div>
+        </header>
+        <div>
+            <div class="question-container">
+              <h1 class="question">
+                What is the chemical value of table salt?
+              </h1>  
+            </div>
+            <div class="options-container">
+                <div class="option">
+                    <p class="option-label">A
+                    </p>
+                    <div class="option-value">
+                        <p>NaCl</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+header{
+    margin-top:20px
+}
+header h4{
+    font-size: 30px;
+}
+.bar{
+    width:300px;
+    height:50px;
+    border:3px solid #aa7bc3;
+    border-radius: 2em;
+}
+.completion{
+    height:100%;
+    width:0%;
+    background-color: #aa7bc3;
+}
+.question-container{
+    margin-top:20px;
+}
+</style>
