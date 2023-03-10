2924afda73266ee8b94f786985a493e061154629
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2023-03-11 00:07:04 +0300

    Update QuizView.vue

--- vue-3-quiz-app/src/views/QuizView.vue
+++ vue-3-quiz-app/src/views/QuizView.vue
@@ -1,6 +1,13 @@
 <script setup>
 import Question from "../components/Question.vue"
 import QuizHeader from "../components/QuizHeader.vue"
+import {useRoute} from "vue-router"
+import {ref} from "vue"
+import quizes from "../data/quizes.json"
+const route= useRoute()
+const quiz= quizes.find(q => q.id === quizId)
+const quizId= parseInt(route.params.id)
+const currentQuestionInex = ref(0);
 </script>
 <template>
     <div>
