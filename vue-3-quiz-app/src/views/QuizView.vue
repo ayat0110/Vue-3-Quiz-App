1f2ec022f15a4815578d85b23b5b49697e4d60ce
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2023-03-11 00:00:32 +0300

    update

--- vue-3-quiz-app/src/views/QuizView.vue
+++ vue-3-quiz-app/src/views/QuizView.vue
@@ -1,36 +1,12 @@
 <script setup>
 import Question from "../components/Question.vue"
+import QuizHeader from "../components/QuizHeader.vue"
 </script>
 <template>
     <div>
-        <header>
-            <h4>Question 1/3</h4>
-            <div class="bar">
-                <div class="completion"></div>
-            </div>
-        </header>
+        <QuizHeader />
         <div>
            <Question />
         </div>
     </div>
 </template>
-<style scoped>
-header{
-    margin-top:20px
-}
-header h4{
-    font-size: 30px;
-}
-.bar{
-    width:300px;
-    height:50px;
-    border:3px solid #afb1ff;
-    border-radius: 2em;
-}
-.completion{
-    height:100%;
-    width:0%;
-    background-color: #afb1ff;
-}
-
-</style>
