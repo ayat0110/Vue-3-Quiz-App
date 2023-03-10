3fa0954f74fb8a4b4fec130267feb9b32362c1bf
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2023-03-10 21:08:33 +0300

    reusable component update

--- vue-3-quiz-app/src/App.vue
+++ vue-3-quiz-app/src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Card from "./components/Card.vue"
 import q from "./data/quizes"
 import {ref, watch} from "vue"
 
@@ -18,13 +19,7 @@ watch(search, () =>{
       <input v-model.trim="search" type="text" placeholder="Search..">
     </header>
     <div class="options-container">
-     <div v-for="quiz in quizes" :key="quiz.id" class="card">
-      <img :src="quiz.img" alt="">
-      <div class="card-text">
-        <h2>{{quiz.name}}</h2>
-        <p>{{quiz.questions.length}} questions</p>
-      </div>
-     </div>
+   <Card  v-for="quiz in quizes" :key="quiz.id" :quiz="quiz"/>
     </div>
   </div>
 </template>
@@ -58,30 +53,5 @@ header input{
   flex-wrap: wrap;
   margin-top:40px;
 }
-.card{
-  width: 310px;
-  overflow: hidden;
-  border-radius:2%;
-  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
-  margin-bottom:35px ;
-  margin-right: 20px;
-  cursor:pointer;
-  padding: 2em;
-  background-color: #f6e5ff;
- color: #9984d4;
-}
 
-.card img{
- width:100%;
-  height:190px;
-  margin:0;
-  border-radius:15px;
-}
-.card .card-text{
-  padding:0 5px;
-}
-.card .card-text h2{
-  font-weight: bold;
-  border-bottom: 0.5px solid gray;
-}
 </style>
---
+++ vue-3-quiz-app/src/components/Card.vue
@@ -0,0 +1,44 @@
+<script setup>
+import {defineProps} from "vue"
+const {quiz} = defineProps(['quiz']);
+
+</script>
+<template>
+    <div>
+         <div class="card">
+      <img :src="quiz.img" alt="">
+      <div class="card-text">
+        <h2>{{quiz.name}}</h2>
+        <p>{{quiz.questions.length}} questions</p>
+      </div>
+     </div>
+    </div>
+</template>
+<style scoped>
+.card{
+  width: 310px;
+  overflow: hidden;
+  border-radius:2%;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  margin-bottom:35px ;
+  margin-right: 20px;
+  cursor:pointer;
+  padding: 2em;
+  background-color: #f6e5ff;
+ color: #9984d4;
+}
+
+.card img{
+ width:100%;
+  height:190px;
+  margin:0;
+  border-radius:15px;
+}
+.card .card-text{
+  padding:0 5px;
+}
+.card .card-text h2{
+  font-weight: bold;
+  border-bottom: 0.5px solid gray;
+}
+</style>
